aac0925cf816be40faba87f2c0a8726e11273f47
Eric Frey <ericlfrey@gmail.com>
2022-11-14 10:54:45 -0600

    Oops coded on main

--- public/main.js
+++ public/main.js
@@ -3,23 +3,27 @@
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
+  fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+const lyricsOnDom = (artist, song) => {
+  getLyrics(artist, song).then((response) => {
+    console.warn(response);
+    document.querySelector('#app').innerHTML = response.lyrics;
+  });
 };
 
-init();
+const startApp = () => {
+  lyricsOnDom('prince', 'purple rain');
+};
+
+startApp();
